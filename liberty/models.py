ca3b11b9d4bc8f54c3e09d3d1a47572645e8e29c
johnyboii14 <perezjonathan1414@gmail.com>
2022-04-07 16:34:42 -0700

    products table

--- liberty/models.py
+++ liberty/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Test(models.Model):
     name = models.CharField(max_length=120)
     age = models.IntegerField()
+
+
+class Products(models.Model):
+    date_added = models.DateTimeField()
+    on_hold_till = models.DateTimeField()
+    item_no = models.CharField(max_length=120)
+    description = models.CharField(max_length=256)
--- web_project/settings.py
+++ web_project/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'), 
+        'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASS'),
-        'HOST': env('DATABASE_HOST'), 
+        'HOST': env('DATABASE_HOST'),
         'PORT': '5432',
     }
 }
