de4b9387f6c287c2969c0ee2385d4cc250938884
johnyboii14 <perezjonathan1414@gmail.com>
2022-04-07 17:15:14 -0700

    full products table

--- liberty/models.py
+++ liberty/models.py
@@ -1,3 +1,5 @@
+from cgi import print_exception
+from email import message_from_string
 from django.db import models
 
 
@@ -11,3 +13,19 @@ class Products(models.Model):
     on_hold_till = models.DateTimeField()
     item_no = models.CharField(max_length=120)
     description = models.CharField(max_length=256)
+    total_units = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2,)
+    avg_cogs = models.DecimalField(max_digits=10, decimal_places=2,)
+    profit = models.DecimalField(max_digits=10, decimal_places=2,)
+    margin = models.DecimalField(max_digits=10, decimal_places=2,)
+    mintage = models.IntegerField()
+    pop_known = models.CharField(max_length=100)
+    ngc_pop = models.IntegerField()
+    pcgs_pop = models.IntegerField()
+    total_pop = models.IntegerField()
+    finer_known = models.IntegerField()
+    highlights = models.TextField()
+    vendor = models.CharField(max_length=256)
+    categories = models.TextField()
+    display_y_n = models.CharField(max_length=256)
+    images_y_n = models.CharField(max_length=256)
