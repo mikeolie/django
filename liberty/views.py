0c502e4ce1ade5de58a855c859e0b9c11e0af6be
Michael Olie <molie@rarecollectiblestv.com>
2022-04-13 14:43:05 -0700

    edited liberty views for sign in properly through django

--- liberty/views.py
+++ liberty/views.py
@@ -1,13 +1,15 @@
 import environ
 from django.http import HttpResponse, JsonResponse
 from django_auth_ldap.backend import LDAPBackend
+from django.contrib.auth import logout, login
 
 env = environ.Env()
 auth = LDAPBackend()
 
 
 def index(request):
-    print(request.user)
+    if request.user.is_authenticated:
+        return JsonResponse({ "message": "authenticated" })
     return HttpResponse("Hello, world. You're at the Liberty index.")
 
 def signIn(request):
@@ -15,6 +17,7 @@ def signIn(request):
     if user is None:
         return JsonResponse({ "message": "unable to get user" }, status=401)
 
+    login(request, user=user, backend='django.contrib.auth.backends.ModelBackend')
     return JsonResponse({ 'message': "Signed In!"})
 
 def signOut(request):
