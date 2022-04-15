b18568615a801070f0141937a67bf9690c271d28
Michael Olie <molie@rarecollectiblestv.com>
2022-04-15 11:49:17 -0700

    script didnt work and was confusing. updated documentation

--- migrate_and_seed.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-python3 manage.py migrate
-fixtures=$(ls seed/)
-while IFS= read -r fixture; do
-    echo -n "Seeding "
-    echo $fixture
-    python3 manage.py loaddata seed/$fixture
-done <<< "$fixtures"
