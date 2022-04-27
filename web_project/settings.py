736e1ec8297fa2732d8953f9a0f059e2f0c50518
Michael Olie <molie@rarecollectiblestv.com>
2022-04-27 10:52:57 -0700

    edited hosts for production

--- web_project/settings.py
+++ web_project/settings.py
@@ -37,8 +37,8 @@ except KeyError as e:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
-                          "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ["*"]
+                
 
 # # # # # --- LDAP SERVER CONFIG --- # # # # #
 
